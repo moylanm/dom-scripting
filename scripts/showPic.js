fc315e62b2d22158526790a3493df4d0c2e19f47
Myles Moylan <moylanm@fastmail.com>
2023-09-16 11:21:35 -0700

    add prepareGallery function for integrating showPic function to onclick events of links

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,8 +1,27 @@
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
+      showPic(this);
+      return false;
+    }
+  }
+}
+
 function showPic(whichPic) {
   var source = whichPic.getAttribute("href");
   var placeholder = document.getElementById("placeholder");
   placeholder.setAttribute("src", source);
+
   var text = whichPic.getAttribute("title");
   var description = document.getElementById("description");
   description.firstChild.nodeValue = text;
 }
+
+window.onload = prepareGallery;
