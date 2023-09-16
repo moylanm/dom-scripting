48a1bbcd22b3d0a54c93bff3872d9b0f63e2fec9
Myles Moylan <moylanm@fastmail.com>
2023-09-16 16:18:21 -0700

    Insert image placeholder and description paragraph elements dynamically

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,3 +1,48 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+function insertAfter(newElement, targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
+}
+
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+
+  var placeholder = document.createElement("img");
+  placeholder.setAttribute("id", "placeholder");
+  placeholder.setAttribute("src", "images/placeholder.jpg");
+  placeholder.setAttribute("alt", "my image gallery");
+  
+  var description = document.createElement("p");
+  description.setAttribute("id", "description");
+
+  var desctext = document.createTextNode("Choose an image");
+  description.appendChild(desctext);
+
+  var gallery = document.getElementById("imagegallery");
+  insertAfter(placeholder, gallery);
+  insertAfter(description, placeholder);
+}
+
 function prepareGallery() {
   if (!document.getElementsByTagName) return false;
   if (!document.getElementById) return false;
@@ -31,4 +76,5 @@ function showPic(whichPic) {
   return true;
 }
 
-window.onload = prepareGallery;
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
