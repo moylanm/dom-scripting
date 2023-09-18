1ff884d088b69d05dce8ec431c302224cf6fac90
Myles Moylan <moylanm@fastmail.com>
2023-09-17 18:07:11 -0700

    Abstract out moveMessage to moveElement

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,21 +1,23 @@
-function moveMessage() {
+function moveElement(elementID, final_x, final_y, interval) {
   if (!document.getElementById) return false;
-  if (!document.getElementById("message")) return false;
+  if (!document.getElementById(elementID)) return false;
 
-  var elem = document.getElementById("message");
+  var elem = document.getElementById(elementID);
   var xpos = parseInt(elem.style.left);
   var ypos = parseInt(elem.style.top);
 
-  if (xpos == 200 && ypos == 100) return true;
+  if (xpos == final_x && ypos == final_y) return true;
 
-  if (xpos < 200) xpos++;
-  if (xpos > 200) xpos--;
-  if (ypos < 100) ypos++;
-  if (ypos > 200) ypos--;
+  if (xpos < final_x) xpos++;
+  if (xpos > final_x) xpos--;
+  if (ypos < final_y) ypos++;
+  if (ypos > final_y) ypos--;
 
   elem.style.left = xpos + "px";
   elem.style.top = ypos + "px";
-  movement = setTimeout("moveMessage()", 10);
+
+  var repeat = "moveElement('" + elementID + "'," + final_x + "," + final_y + "," + interval + ")";
+  movement = setTimeout(repeat, interval);
 }
 
 function positionMessage() {
@@ -26,7 +28,7 @@ function positionMessage() {
   elem.style.position = "absolute";
   elem.style.left = "50px";
   elem.style.top = "100px";
-  movement = setTimeout("moveMessage()", 5000);
+  moveElement("message", 125, 25, 20);
 }
 
 addLoadEvent(positionMessage);
