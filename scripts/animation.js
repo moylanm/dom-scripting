807126ac12a30e67e29f6f052e2beb097b93032b
Myles Moylan <moylanm@fastmail.com>
2023-09-17 17:32:33 -0700

    Experiment with changing the position of an element

---
+++ scripts/animation.js
@@ -0,0 +1,20 @@
+function moveMessage() {
+  if (!document.getElementById) return false;
+  if (!document.getElementById("message")) return false;
+
+  var elem = document.getElementById("message");
+  elem.style.left = "200px";
+}
+
+function positionMessage() {
+  if (!document.getElementById) return false;
+  if (!document.getElementById("message")) return false;
+
+  var elem = document.getElementById("message");
+  elem.style.position = "absolute";
+  elem.style.left = "50px";
+  elem.style.top = "100px";
+  movement = setTimeout("moveMessage()", 5000);
+}
+
+addLoadEvent(positionMessage);
